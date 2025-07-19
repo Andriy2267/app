3129b043b2e8e49a43796a29c403cfcba944c9c4
Andriy2267 <andrijcopek696@gmail.com>
2025-07-19 18:01:03 +0300

    Project templates have been added to the project

--- main/views.py
+++ main/views.py
@@ -3,7 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, "main/index.html")
+    context = {
+        "title": "Home",
+        "content": "Home - furniture shop"
+    }
+    return render(request=request, template_name="main/index.html", context=context)
 
 def about(request):
-    return HttpResponse("About page")
+    context = {
+        "title": "About us",
+        "content": "Home - about us",
+        "text_on_page": "Hello world! My name is Andrii and I am from Ukraine and it is my Home shop!"
+    }
+
+    return render(request=request, template_name="main/about.html", context=context)
