96d4e56fcc30106eb55721a3a336ae2fcf8b1e89
Andriy2267 <andrijcopek696@gmail.com>
2025-07-21 14:59:15 +0300

    goods app has been added to the project

---
+++ goods/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def catalog(request):
+    return render(request=request, template_name='goods/catalog.html')
+
+def product(request):
+    return render(request=request, template_name='goods/product.html')
